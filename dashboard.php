2c3e0c8a714dc78c8b8414ffdb9d215c2f3f0e42
KOEMIN <raufahafid87@gmail.com>
2025-05-23 18:34:54 +0700

    Add files via upload
    mengganti extensi menjadi .php

---
+++ dashboard.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presensi Mahasiswa</title>
+    <link rel="icon" type="png" href="knowledge.png">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <table width="100%">
+                <tr>
+                    <td width="80%" style="text-align: left; font-size: 35px;">
+                        <h2>Selamat Datang</h2>
+                    </td>
+                    <td>
+                        <h3 style="text-align: right;">Presensi</h3>
+                        <h3 style="text-align: right;">Mahasiswa</h3>
+                    </td>
+                    <td width="3%">
+                        <img src="knowledge.png" width="45px">
+                    </td>
+                </tr>
+            </table>
+        </header>
+
+        <div class="main-content">
+
+        <section class="content">
+
+        <table width="100%" height="700px";>
+        <tr height="15%" style="vertical-align: bottom; text-align: center;">
+            
+            <td colspan="3">
+                <h1 style="font-size: 70px; padding-top: 45px;">
+                    Mau Nyari Apa Hari Ini?
+                </h1>
+            </td>
+        </tr>
+
+        <tr height="70%" style="text-align: center; font-size: 15px; color: #3D8D7A;">
+            <td width="33%">
+                <div class="menu">
+                    <a href="absen.html">
+                        <img src="higher-education.png" width="100px" height="100px">
+                    </a>
+                    <p style="padding-top:3%; font-size: 20px;">Status Absensi</p>
+                </div>
+            </td>
+            <td width="33%">
+                <div class="menu">
+                <a href="jadwal.html">
+                    <img src="training.png" width="100px" height="100px">
+                </a>
+                <p  style="padding-top:3%; font-size: 20px;">Jadwal Perkuliahan</p>
+                </div>
+            </td>
+            <td width="33%">
+                <div class="menu">
+                <a href="kelas.html">
+                    <img src="education.png" width="100px" height="100px">
+                </a>
+                <p  style="padding-top:3%; font-size: 20px;">Informasi Kelas</p>
+                </div>
+            </td>
+        </tr>
+
+        <tr height="15%" style="text-align: center; vertical-align: bottom;">
+            <td colspan="3">
+            </td>
+        </tr>
+        </table>
+        </section>
+        </div>
+
+        <footer>
+            <a href="#">
+                <img src="sign-out-alt.png" width="35px" height="35px">
+            </a>
+        </footer>
+    </div>
+
+</body>
+</html>
